b7dc58e4c3a00db12c87670cda8530aaf2a8d6ca
Jamie Sullivan <jamiesullivan523@gmail.com>
2024-03-02 17:24:00 +0000

    Setup up autocomplete search locations

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
-import { useRef, useEffect, useState, ChangeEvent } from 'react';
+import { useRef, useEffect, useState, SyntheticEvent } from 'react';
 import mapboxgl, { Map } from 'mapbox-gl';
 import axios from 'axios';
 import { v4 as uuid } from 'uuid';
+import { Autocomplete, TextField, Stack } from '@mui/material';
 
 import './App.css';
-import Input from './components/Input';
 
 const token = process.env.REACT_APP_MAPBOX_KEY || '';
 
 mapboxgl.accessToken = token;
+
+interface Location {
+  name: string;
+  place_formatted: string;
+}
+
+interface LocationInput {
+  active: string;
+  start: string;
+  end: string;
+}
 
 function App() {
   const map = useRef<Map | null>(null);
@@ -16,7 +27,15 @@ function App() {
   const [lng, setLng] = useState(-70.9);
   const [lat, setLat] = useState(42.35);
   const [zoom, setZoom] = useState(9);
-  const [locationText, setLocationText] = useState('');
+  const [startLocationOptions, setStartLocationOptions] = useState<Location[]>(
+    []
+  );
+  const [endLocationOptions, setEndLocationOptions] = useState<Location[]>([]);
+  const [locationInput, setLocationInput] = useState<LocationInput>({
+    active: '',
+    start: '',
+    end: '',
+  });
   const sessionId = uuid();
 
   useEffect(() => {
@@ -32,26 +51,43 @@ function App() {
   }, []);
 
   useEffect(() => {
-    if (locationText) {
+    if (locationInput.active) {
       fetch();
     }
-  }, [locationText]);
+  }, [locationInput.active, locationInput.start, locationInput.end]);
 
-  const handleOnChange = (e: ChangeEvent) => {
-    const element = e.currentTarget as HTMLInputElement;
-    console.log(element.value);
-    setLocationText(element.value);
+  const handleLocationChange = (e: SyntheticEvent<Element, Event>) => {
+    const element = e.target as HTMLInputElement;
+    const { id, value } = element;
+    setLocationInput({
+      ...locationInput,
+      active: id,
+      [id]: value,
+    });
   };
 
   const fetch = () => {
+    const key = locationInput.active as keyof LocationInput;
+    if (!locationInput[key]) return;
     axios
       .get(
-        `https://api.mapbox.com/search/searchbox/v1/suggest?q=${locationText}&access_token=${token}&session_token=${sessionId}`
+        `https://api.mapbox.com/search/searchbox/v1/suggest?q=${locationInput[key]}&access_token=${token}&session_token=${sessionId}`
       )
       .then((res) => {
-        console.log(res.data);
+        if (locationInput.active === 'start') {
+          setStartLocationOptions(res.data.suggestions);
+        }
+        if (locationInput.active === 'end') {
+          setEndLocationOptions(res.data.suggestions);
+        }
+        console.log(res.data.suggestions);
       });
   };
+
+  // const selectedValues = useMemo(
+  //   () => startLocationOptions.filter((v) => v.selected),
+  //   [allValues]
+  // );
 
   return (
     <div className='App'>
@@ -60,14 +96,43 @@ function App() {
         className='map-container'
       />
       <div className='location-container'>
-        <Input
-          name='startPoint'
-          onChange={handleOnChange}
-        />
-        <Input
-          name='destination'
-          onChange={handleOnChange}
-        />
+        <Stack
+          spacing={2}
+          sx={{ width: 300 }}
+        >
+          <Autocomplete
+            id='start'
+            value={locationInput.start}
+            onInputChange={handleLocationChange}
+            freeSolo
+            options={startLocationOptions.map(
+              (location: Location) =>
+                `${location.name} ${location.place_formatted}`
+            )}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label='Start Location'
+              />
+            )}
+          />
+          <Autocomplete
+            id='end'
+            value={locationInput.end}
+            onInputChange={handleLocationChange}
+            freeSolo
+            options={endLocationOptions.map(
+              (location: Location) =>
+                `${location.name} ${location.place_formatted}`
+            )}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label='End Location'
+              />
+            )}
+          />
+        </Stack>
       </div>
     </div>
   );
